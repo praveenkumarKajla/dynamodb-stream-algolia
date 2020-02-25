88dff438510e38e0022b74f982791bcc05935f5a
praveenKajla <praveenkajla8099@gmail.com>
2020-02-25 19:25:15 +0530

    async-aswait, update algoliia 4.0, error handling

--- src/utils/algolia-wrapper.js
+++ src/utils/algolia-wrapper.js
@@ -4,45 +4,66 @@ module.exports = (appId, adminKey) => {
 
   const client = algoliasearch(appId, adminKey);
   return {
-    add: ({ index_name, id, body }) => new Promise((resolve, reject) => {
+    add: async ({ index_name, id, body }) => {
       let index = client.initIndex(index_name);
-      index.addObject({ ...body, objectID: id }, (err, content) => {
-        if (err) reject(err)
+      try {
+        let content = await index.saveObject({ ...body, objectID: id })
         console.log('success');
-        resolve(content)
-      });
-    }),
-    index: ({ index_name, id, body }) => new Promise((resolve, reject) => {
-      let index = client.initIndex(index_name);
-      index.partialUpdateObject({ ...body, objectID: id }, true, (err, content) => {
-        if (err) reject(err)
-        console.log('success');
-        resolve(content)
-      });
-    }),
-    remove: ({ index_name, id }) => new Promise((resolve, reject) => {
-      let index = client.initIndex(index_name);
-      index.deleteObject(id, (error, response) => {
-        if (error) reject(error)
-        console.log('success');
-        resolve(response)
-      })
-    }),
+        return content
+      }
+      catch (e) {
+        throw new Error(e.message)
+      }
 
-    exists: ({ index_name }) => new Promise((resolve, reject) => {
+    },
+    index: async ({ index_name, id, body }) => {
       let index = client.initIndex(index_name);
-      index.exists({ index }, (error, response) => {
-        if (error) reject(error)
-        resolve(response)
-      })
-    }),
-    indicesDelete: (index_name = '_all') => new Promise((resolve, reject) => {
-      client.deleteIndex(index_name, (err, content) => {
-        if (err) reject(err)
+      try {
+        let content = await index.partialUpdateObject({ ...body, objectID: id }, {
+          createIfNotExists: true
+        })
         console.log('success');
-        resolve(content)
+        return content
+      }
+      catch (e) {
+        throw new Error(e.message)
+      }
 
-      });
-    })
+
+    },
+    remove: async ({ index_name, id }) => {
+      let index = client.initIndex(index_name);
+      try {
+        let content = await index.deleteObject(id)
+        console.log('success');
+        return content
+      }
+      catch (e) {
+        throw new Error(e.message)
+      }
+    },
+
+    exists: async ({ index_name }) => {
+      let index = client.initIndex(index_name);
+      try {
+        let content = await index.exists()
+        console.log('success');
+        return content
+      }
+      catch (e) {
+        throw new Error(e.message)
+      }
+    },
+    indicesDelete: async (index_name = '_all') => {
+      let index = client.initIndex(index_name);
+      try {
+        let content = await index.delete()
+        console.log('success');
+        return content
+      }
+      catch (e) {
+        throw new Error(e.message)
+      }
+    }
   }
 }
